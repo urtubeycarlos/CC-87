113c34509dffa39c862eb0e3adc904e87edf6dd7
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2021-11-13 23:30:59 -0300

    added jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  def page_not_found
+    render json: '418 I\'m a teapot', status: 418
+  end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header.split(' ').last if header
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: '401 Unauthorized', status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: '401 Unauthorized', status: :unauthorized
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # -----> routes <-----
+
+  # -----> end of routes <-----
+  # This is the last one for custom page not found. Important! -> LAST
+  match '*unmatched', to: 'application#page_not_found', via: :all
 end
