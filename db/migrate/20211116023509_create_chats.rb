a787804ea79d62afafae0fde90b1188ef56b00df
eogieglo <eric.ogieglo@gmail.com>
2021-11-17 12:51:43 -0300

    Modified migration message y chat

--- db/migrate/20211116023509_create_chats.rb
+++ db/migrate/20211116023509_create_chats.rb
@@ -1,9 +1,9 @@
 class CreateChats < ActiveRecord::Migration[6.1]
   def change
     create_table :chats do |t|
-      t.string :user1
-      t.string :user2
-      t.string :state
+      t.string :user1, null: false
+      t.string :user2, null: false
+      t.string :state, null: false
 
       t.timestamps
     end
--- db/migrate/20211116025331_create_messages.rb
+++ db/migrate/20211116025331_create_messages.rb
@@ -1,9 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
-      t.string :user_id
-      t.text :detail
-      t.boolean :modified
+      t.string :user_id, null: false
+      t.text :detail, null: false
+      t.boolean :modified, null: false
 
       t.timestamps
     end
