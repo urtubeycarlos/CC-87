1e88759d2d6e26d73676a9ab443cea767601ccd5
Clara Maffei <claramaffei@gmail.com>
2021-11-21 02:55:18 -0300

    User list documentation and testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # -----> routes <-----
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :users, only: %i[index]
   post '/auth/login', to: 'authentication#login'
   # -----> end of routes <-----
   # This is the last one for custom page not found. Important! -> LAST
-  match '*unmatched', to: 'application#page_not_found', via: :all
+  #match '*unmatched', to: 'application#page_not_found', via: :all
 end
