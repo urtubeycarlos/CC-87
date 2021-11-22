2862ba8329e386e57049da1a23e8b86a55ae743b
Clara Maffei <claramaffei@gmail.com>
2021-11-22 00:07:08 -0300

    User Rspec test cases

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,36 @@
-require 'swagger_helper'
+require 'rails_helper'
 
-RSpec.describe 'users', type: :request do
-  path '/users' do
-    get('list users') do
-      response(200, 'successful') do
+RSpec.describe 'Users', type: :request do
+  # GET
+  describe 'GET users' do
+    context 'when user is authenticated' do
+      let(:users) { create_list(:user, 5) }
+      before {
+        token = JsonWebToken.encode(user_id: users.first.id)
+        get '/users', params: {}, headers: { 'Authorization' => token }
+      }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
+      it 'returns status code 200' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns all users' do
+        expect(JSON.parse(response.body).size).to eq(5)
+      end
+
+      it 'returns one user' do
+        get "/users/#{users.first.id}", params: {}, headers: { 'Authorization' => JsonWebToken.encode(user_id: users.first.id) }
+        expect(JSON.parse(response.body)['first_name']).to eq(users.first.first_name)
+        expect(JSON.parse(response.body)['last_name']).to eq(users.first.last_name)
+        expect(JSON.parse(response.body)['email']).to eq(users.first.email)
+      end
+
+    end
+
+    context 'when user is NOT authenticated' do
+      it 'returns status code 401 - unauthorized' do
+        get '/users'
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
